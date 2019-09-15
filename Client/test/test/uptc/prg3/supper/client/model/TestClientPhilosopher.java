21b5e6a91b05dce7b14da0fa4a836a879296eb59
WiseUnicornOfWisdom <luis.quiroga01@uptc.edu.co>
2019-09-14 22:17:51 -0500

    Ordering more files...

--- Client/test/test/uptc/prg3/supper/client/model/TestClientPhilosopher.java
+++
@@ -1,9 +0,0 @@
-package test.uptc.prg3.supper.client.model;
-
-import com.uptc.prg3.supper.client.controller.ClientController;
-
-public class TestClientPhilosopher {
-	public static void main(String[] args) {
-		ClientController.getInstance();
-	}
-}
--- Client/test/test/uptc/prg3/supper/client/view/TestMainFrame.java
+++
@@ -1,9 +0,0 @@
-package test.uptc.prg3.supper.client.view;
-
-import com.uptc.prg3.supper.client.view.MainFrame;
-
-public class TestMainFrame {
-    public static void main (String[] args) {
-        new MainFrame();
-    }
-}
